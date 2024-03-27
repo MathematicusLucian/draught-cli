9c1b96fe89601d68110d5de69d161fe6784e21c8
k1n4kut4 <recruitlukejones@gmail.com>
2024-03-27 01:28:08 +0000

    Created NUnit test for Square

--- Tests.cs
+++ Tests.cs
@@ -29,5 +29,28 @@ namespace draught_cli.Tests
             // Assert
             ClassicAssert.AreEqual("Rectangle (10,10) width=30 height=40", result);
         }
+
+        [Test]
+        public void CreateSquare_ShouldNotBeNull()
+        {
+            // Arrange & Act
+            Square square = new Square { X = 0, Y = 0, Size = 15 };
+            
+            // Assert
+            ClassicAssert.IsNotNull(square);
+        }
+
+        [Test]
+        public void SquareDraw_ShouldReturnExpectedString()
+        {
+            // Arrange
+            Square square = new Square { X = 0, Y = 0, Size = 15 };
+            
+            // Act
+            string result = square.Draw();
+            
+            // Assert
+            ClassicAssert.AreEqual("Square (0,0) size=15", result);
+        }
     }
 }
